7381f8d49e7a92ea9326d27a3b9cdf09b4f25ee4
Federico <polli.f@outlook.it>
2021-01-20 17:04:19 +0100

    Developed classes Board and Position

--- src/main/java/dssc/project/freedom/Board.java
+++ src/main/java/dssc/project/freedom/Board.java
@@ -1,5 +1,16 @@
 package dssc.project.freedom;
 
+import java.util.HashMap;
+
 public class Board {
+
+    private HashMap<Position, Stone> board;
+    private final int boardSize;
+
+    public Board(int boardSize) {
+        this.boardSize = boardSize;
+
+    }
+
 
 }
--- src/main/java/dssc/project/freedom/Position.java
+++ src/main/java/dssc/project/freedom/Position.java
@@ -1,6 +1,25 @@
 package dssc.project.freedom;
 
 public class Position {
-    private int x, y;
+    private final int x, y;
 
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + 59 * y;
+    }
 }
