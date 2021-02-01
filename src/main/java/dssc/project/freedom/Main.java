1bfc7fb58941dc356cc63642fb2dcff74eaa4871
Angela Carraro <angela_carraro@hotmail.it>
2021-02-01 17:29:45 +0100

    Added method getInteger in Main

--- src/main/java/dssc/project/freedom/Main.java
+++ src/main/java/dssc/project/freedom/Main.java
@@ -8,21 +8,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the board size (minimum 4):");
-        int boardSize = 4;
-        boolean flag = true;
-        while (flag){
-            try {
-                boardSize = in.nextInt();
-                if (boardSize >= 4)
-                    flag = false;
-            } catch (InputMismatchException e){
-                System.out.println("Boardsize must be an integer >= 4");
+        int boardSize;
+        boolean flag;
+        do {
+            flag = false;
+            boardSize = getInteger(in);
+            if (boardSize < 4) {
+                flag = true;
+                System.out.println("The size of the board must be an integer >= 4");
             }
-        }
+        } while (flag);
         Game game = new Game(boardSize);
         Colour colour;
         for (int i = 1; i <= boardSize*boardSize; ++i){
-            if (i%2 != 0){
+            if (i % 2 != 0){
                 System.out.println("White it's your turn!");
                 colour = Colour.WHITE;
             } else {
@@ -32,23 +31,36 @@ public class Main {
             Position current;
             do {
                 System.out.println("Enter the x and y coordinates of the stone:");
-                // add some print
-                int x = in.nextInt();
-                int y = in.nextInt();
+                int x = getInteger(in);
+                int y = getInteger(in);
                 current = Position.at(x, y);
-            }while (!game.isMoveValid(current));
-            if (i == boardSize*boardSize){
+            } while (!game.isMoveValid(current));
+            if (i == boardSize * boardSize){
                 if (!game.isLastMoveConvenient(current, colour)){
-                    System.out.println("Do you want to do the last move?(0 = yes, 1= no)");
-                    int d = in.nextInt();
-                    if (d==1){
+                    System.out.println("Do you want to do the last move? (0 = yes, 1 = no)");
+                    int d = getInteger(in);
+                    if (d != 0){
                         break;
                     }
                 }
             }
             game.play(current, colour);
         }
-
         game.winner();
+    }
+
+    private static int getInteger(Scanner in) {
+        boolean flag = false;
+        int i = 0;
+        do {
+            try {
+                i = in.nextInt();
+                flag = false;
+            } catch (InputMismatchException e) {
+                System.out.println("You didn't enter an integer!");
+                flag = true;
+            }
+         } while(flag);
+        return i;
     }
 }
