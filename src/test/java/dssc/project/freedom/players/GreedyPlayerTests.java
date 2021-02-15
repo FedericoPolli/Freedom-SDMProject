840c559b1c699ca8e44e6fb722e7cceffdb8664b
Angela Carraro <angela_carraro@hotmail.it>
2021-02-15 11:25:03 +0100

    Testing that GreedyPlayer has to put the fifth stone

--- src/test/java/dssc/project/freedom/players/GreedyPlayerTests.java
+++ src/test/java/dssc/project/freedom/players/GreedyPlayerTests.java
@@ -72,4 +72,24 @@ public class GreedyPlayerTests {
         GreedyPlayer greedyPlayer = new GreedyPlayer("White", WHITE);
         assertEquals(at(3, 4), greedyPlayer.getPlayerPosition());
     }
+
+    @Test
+    public void hasToPutFifthStone() {
+        int boardSize = 5;
+        Board board = new Board(boardSize);
+        for (int i = 1; i <= 4; ++i) {
+            for (int j = 1; j <= 4; ++j) {
+                if (i == j) {
+                    board.updateStoneAt(at(i, j), WHITE);
+                    board.updateStoneAt(at(i, j + 1), BLACK);
+                }
+            }
+        }
+        board.updateStoneAt(at(5, 4), BLACK);
+        board.updateStoneAt(at(3, 5), WHITE);
+        GreedyPlayer.setBoard(board);
+        GreedyPlayer.setPrevious(at(4, 5));
+        GreedyPlayer greedyPlayer = new GreedyPlayer("White", WHITE);
+        assertEquals(at(5, 5), greedyPlayer.getPlayerPosition());
+    }
 }
