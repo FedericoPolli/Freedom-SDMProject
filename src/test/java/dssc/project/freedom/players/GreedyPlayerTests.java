d2dadfae30013659f788314d095816fa2b4fda5b
Angela Carraro <angela_carraro@hotmail.it>
2021-02-15 10:59:39 +0100

    Testing that GreedyPlayer doesn't choose the fifth Stone of the row

--- src/test/java/dssc/project/freedom/players/GreedyPlayerTests.java
+++ src/test/java/dssc/project/freedom/players/GreedyPlayerTests.java
@@ -1,14 +1,12 @@
 package dssc.project.freedom.players;
 
 import dssc.project.freedom.Board;
-import dssc.project.freedom.CommandLineGame;
 import org.junit.jupiter.api.Test;
 
 import static dssc.project.freedom.Colour.BLACK;
 import static dssc.project.freedom.Colour.WHITE;
 import static dssc.project.freedom.Position.at;
-import static dssc.project.freedom.CommandLineGame.*;
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GreedyPlayerTests {
 
@@ -28,5 +26,23 @@ public class GreedyPlayerTests {
         GreedyPlayer.setPrevious(at(3, 4));
         GreedyPlayer greedyPlayer = new GreedyPlayer("White", WHITE);
         assertEquals(at(4, 4), greedyPlayer.getPlayerPosition());
+    }
+
+    @Test
+    public void doNotChooseFifthStone() {
+        int boardSize = 5;
+        Board board = new Board(boardSize);
+        for (int i = 1; i <= 4; ++i) {
+            for (int j = 1; j <= 4; ++j) {
+                if (i == j) {
+                    board.updateStoneAt(at(i, j), WHITE);
+                    board.updateStoneAt(at(i, j + 1), BLACK);
+                }
+            }
+        }
+        GreedyPlayer.setBoard(board);
+        GreedyPlayer.setPrevious(at(4, 5));
+        GreedyPlayer greedyPlayer = new GreedyPlayer("White", WHITE);
+        assertNotEquals(at(5, 5), greedyPlayer.getPlayerPosition());
     }
 }
