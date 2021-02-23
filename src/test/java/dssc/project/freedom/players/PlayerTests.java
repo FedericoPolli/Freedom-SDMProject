8e2c89681e17cc7230a58694e793dedbcbfb123e
Alice <alice.vegliach@gmail.com>
2021-02-23 17:50:23 +0100

    Added tests for Player

---
+++ src/test/java/dssc/project/freedom/players/PlayerTests.java
@@ -0,0 +1,41 @@
+package dssc.project.freedom.players;
+
+import dssc.project.freedom.basis.Colour;
+import dssc.project.freedom.basis.Position;
+import org.junit.jupiter.api.Test;
+
+import static dssc.project.freedom.basis.Position.at;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PlayerTests {
+    private class TestPlayer extends Player {
+
+        public TestPlayer(String name, Colour colour) {
+            super(name, colour);
+        }
+
+        @Override
+        public Position getPlayerPosition() {
+            return at(1, 1);
+        }
+    }
+
+    private Player player = new TestPlayer("Player One", Colour.WHITE);
+
+    @Test
+    void testGetName(){
+        assertEquals("Player One", player.getName());
+    }
+
+    @Test
+    void testGetColour(){
+        assertEquals(Colour.WHITE, player.getColour());
+    }
+
+    @Test
+    void testChangeName(){
+        player.changeName("New name for player One");
+        assertEquals("New name for player One", player.getName());
+    }
+
+}
