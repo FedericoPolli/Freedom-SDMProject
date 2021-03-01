196f54e273bd8b758bd0f867189a5d8965137947
Alice <alice.vegliach@gmail.com>
2021-03-01 16:24:32 +0100

    Refactor of Board

--- src/main/java/dssc/project/freedom/basis/Board.java
+++ src/main/java/dssc/project/freedom/basis/Board.java
@@ -44,27 +44,27 @@ public class Board {
 
     /**
      * Returns the {@link Stone} in the {@link Board} at the given {@link Position}.
-     * @param p The Position of the Stone to be retrieved.
+     * @param position The Position of the Stone to be retrieved.
      * @return The Stone at the given Position.
      */
-    private Stone getStoneAt(Position p) {
-        return board.get(p);
+    private Stone getStoneAt(Position position) {
+        return board.get(position);
     }
 
     /**
      * Colours the {@link Stone} in the given {@link Position} in the {@link Board},
      * colouring it with the given {@link Colour}.
-     * @param p The Position of the Stone to be updated.
-     * @param c The Colour to be assigned to the Stone.
+     * @param position The Position of the Stone to be coloured.
+     * @param colour The Colour to be assigned to the Stone.
      */
-    public void colourStoneAt(Position p, Colour c) {
-        getStoneAt(p).makeOfColour(c);
+    public void colourStoneAt(Position position, Colour colour) {
+        getStoneAt(position).makeOfColour(colour);
     }
 
     /**
-     * Counts the "live" {@link Stone}s of the player of the given {@link Colour}.
-     * @param colour The Colour of the player.
-     * @return The number of "live" Stones for that player.
+     * Counts the "live" {@link Stone}s of the given {@link Colour}.
+     * @param colour The considered Colour.
+     * @return The number of "live" Stones for that Colour.
      */
     public int countLiveStones(Colour colour) {
         return (int) board.values()
@@ -86,59 +86,60 @@ public class Board {
     }
 
     /**
-     * Finds all the free {@link Position}s that are adjacent to the given {@link Position}.
-     * @param pos The Position to be checked.
-     * @return The free Positions adjacent to the given one.
+     * Finds all the free {@link Position}s that are adjacent to the given {@link Position}
+     * and returns them in a {@link List}.
+     * @param position The Position to be checked.
+     * @return A List with the free Positions adjacent to the given one.
      */
-    public List<Position> getFreeAdjacentPositions(Position pos) {
+    public List<Position> getFreeAdjacentPositions(Position position) {
         return getFreePositions().stream()
-                .filter(p -> p.isInAdjacentPositions(pos))
+                .filter(p -> p.isInAdjacentPositions(position))
                 .collect(Collectors.toList());
     }
 
     /**
-     * Checks if all the {@link Position}s adjacent to the given one are free.
-     * @param pos The Position to be checked.
-     * @return true if all the Positions adjacent to the given one are occupied, false otherwise.
+     * Checks if at least one of the {@link Position}s adjacent to the given one is free.
+     * @param position The Position to be checked.
+     * @return true if at least one Positions adjacent to the given one is free, false otherwise.
      */
-    public boolean areAdjacentPositionFree(Position pos) {
-        return !getFreeAdjacentPositions(pos).isEmpty();
+    public boolean areAdjacentPositionFree(Position position) {
+        return !getFreeAdjacentPositions(position).isEmpty();
     }
 
     /**
      * Checks if a {@link Stone} has already been placed in the {@link Position}
      * taken as input. This is done by checking the {@link Colour} of the {@link
      * Stone}: it has not been placed only if the {@link Colour} is <code>NONE</code>.
-     * @param current The Position to be checked.
+     * @param position The Position to be checked.
      * @return true if the Stone has already a Colour, false otherwise.
      */
-    public boolean stoneIsAlreadyPlacedAt(Position current) {
-        return !getStoneAt(current).isOfColour(Colour.NONE);
+    public boolean stoneIsAlreadyPlacedAt(Position position) {
+        return !getStoneAt(position).isOfColour(Colour.NONE);
     }
 
     /**
      * Checks in the whole board for occurrences of exactly four {@link Stone}s of
-     * the same {@link Colour} in a row, in the direction specified by the input
-     * {@link Direction}, and makes the {@link Stone}s "live".
-     * @param dir The direction in which to move.
+     * the same {@link Colour} in a row, in the {@link Direction} specified by the input,
+     * and makes the {@link Stone}s "live".
+     * @param direction The direction in which to move.
      */
-    private void check4StonesInDirection(Direction dir) {
+    private void check4StonesInDirection(Direction direction) {
         for (Position current : board.keySet()) {
-            Position previous = current.moveInDirectionWithStep(dir, -1);
+            Position previous = current.moveInDirectionWithStep(direction, -1);
             if (positionIsInsideTheBoard(previous) && areStonesOfSameColourAt(current, previous))
                 continue;
-            if (countStonesInRow(dir, current) == 4)
-                setStonesInRowOf4Live(dir, current);
+            if (countStonesInRow(direction, current) == 4)
+                setStonesInRowOf4Live(direction, current);
         }
     }
 
     /**
      * Checks if the input {@link Position} is inside the {@link Board} or not.
-     * @param p The Position to be checked.
+     * @param position The Position to be checked.
      * @return true if the Position is in the Board, false otherwise.
      */
-    public boolean positionIsInsideTheBoard(Position p) {
-        return board.containsKey(p);
+    public boolean positionIsInsideTheBoard(Position position) {
+        return board.containsKey(position);
     }
 
     /**
@@ -156,14 +157,14 @@ public class Board {
      * Counts the number of {@link Stone}s of the same {@link Colour} in a row
      * starting from the <code>startingPosition</code> {@link Position}, according to the
      * direction specified by the input {@link Direction}.
-     * @param dir     The direction in which to move.
+     * @param direction     The direction in which to move.
      * @param startingPosition The starting Position.
      * @return The number of Stones of the same Colour adjacent to the given one.
      */
-    private int countStonesInRow(Direction dir, Position startingPosition) {
+    private int countStonesInRow(Direction direction, Position startingPosition) {
         int stonesInRow = 1;
-        for (int i = 1; i < 5; ++i) {
-            Position current = startingPosition.moveInDirectionWithStep(dir, i);
+        for (int step = 1; step < 5; ++step) {
+            Position current = startingPosition.moveInDirectionWithStep(direction, step);
             if (positionIsInsideTheBoard(current) && areStonesOfSameColourAt(startingPosition, current))
                 stonesInRow++;
             else
@@ -175,13 +176,13 @@ public class Board {
     /**
      * Sets as "live" the {@link Stone}s that are part of a row of exactly four {@link Stone}s
      * of the same {@link Colour}, starting from the {@link Position} <code>startingPosition</code>,
-     * in the direction given by the input <code>xDir</code> and <code>yDir</code>.
-     * @param dir     The direction in which to move.
+     * in the {@link Direction} given by the input.
+     * @param direction The direction in which to move.
      * @param startingPosition The starting Position.
      */
-    private void setStonesInRowOf4Live(Direction dir, Position startingPosition) {
-        for (int i = 0; i < 4; ++i) {
-            Position current = startingPosition.moveInDirectionWithStep(dir, i);
+    private void setStonesInRowOf4Live(Direction direction, Position startingPosition) {
+        for (int step = 0; step < 4; ++step) {
+            Position current = startingPosition.moveInDirectionWithStep(direction, step);
             getStoneAt(current).changeLiveStatusTo(true);
         }
     }
@@ -199,31 +200,32 @@ public class Board {
     }
 
     /**
-     * Returns the maximum number of {@link Stone}s of the same {@link Colour}
+     * Returns the number of {@link Stone}s of the same {@link Colour}
      * of the given one in a certain row starting from the given {@link Stone}
      * for all the {@link Direction}s.
-     * @param p The Position of the given Stone.
+     * @param position The Position of the given Stone.
      * @param colour The Colour of the given Stone.
      * @return The maximum number of Stones of the same Colour in a row.
      */
-    public List<Integer> getNumberOfStonesInRowForAllDirections(Position p, Colour colour) {
-        colourStoneAt(p, colour);
+    public List<Integer> getNumberOfStonesInRowForAllDirections(Position position, Colour colour) {
+        colourStoneAt(position, colour);
         List<Integer> maximumNumberOfStonesInARow = new ArrayList<>();
         for (Direction dir : Direction.values())
-            maximumNumberOfStonesInARow.add(countStonesInRow(dir, p));
-        colourStoneAt(p, Colour.NONE);
+            maximumNumberOfStonesInARow.add(countStonesInRow(dir, position));
+        colourStoneAt(position, Colour.NONE);
         return maximumNumberOfStonesInARow;
     }
 
     /**
-     * It searches in all eight {@link dssc.project.freedom.basis.Direction}s for
-     * the maximum number of {@link dssc.project.freedom.basis.Stone}s in a row
-     * that would be obtained by playing in the given {@link Position}.
-     * @param p The Position on which to perform the search.
-     * @return The maximum number of Stones in a row for the given Position.
+     * Returns the maximum number of {@link Stone}s in a row of a given
+     * {@link Colour}, starting form a given {@link Position} among all the
+     * possible {@link Direction}s.
+     * @param position The Position on which to perform the search.
+     * @param colour The Colour of the given Stone.
+     * @return The maximum number of Stones of the same Colour in a row for the given Position.
      */
-    public Integer getMaximumNumberOfStonesInARow(Position p, Colour colour) {
-        return getNumberOfStonesInRowForAllDirections(p, colour)
+    public Integer getMaximumNumberOfStonesInARow(Position position, Colour colour) {
+        return getNumberOfStonesInRowForAllDirections(position, colour)
                 .stream()
                 .max(Comparator.naturalOrder())
                 .get();
